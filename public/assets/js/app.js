e4238173548a6fc14e9aa8c2a2a6516d2325d68e
Ryan Rodwell <ryanrodwell@gmail.com>
2017-09-12 17:35:35 -0400

    changing nav and footer

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -16,6 +16,18 @@ $(document).ready(function() {
 
     $('select').material_select();
 
+    $('.dropdown-button').dropdown({
+            inDuration: 300,
+            outDuration: 225,
+            constrainWidth: false,
+            hover: false,
+            gutter: 0,
+            belowOrigin: true,
+            alignment: 'left',
+            stopPropagation: false
+        }
+    );
+
     $('.slider').slider({
         height: '600px',
         indicators: false
