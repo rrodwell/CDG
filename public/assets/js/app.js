defa11ef9456074658fd14467f735b381c1b9e34
Ryan Rodwell <ryanrodwell@gmail.com>
2017-09-13 01:34:33 -0400

    modal image lightbox test

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -76,7 +76,7 @@ $(document).ready(function() {
     // });
 
     $('.alphabet').click(function() {
-        $('#modal1').modal('open');
+        $('#myModal').modal('open');
     });
 
     $('.remixA').click(function() {
@@ -99,3 +99,42 @@ $(document).ready(function() {
     console.log($(".contact-us").height());
     $(".find-us").css("height", $(".connect-section").height());
 });
+
+
+function openModal() {
+    document.getElementById('myModal').style.display = "block";
+}
+
+function closeModal() {
+    document.getElementById('myModal').style.display = "none";
+    $('#myModal').modal('close');
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("demo");
+    var captionText = document.getElementById("caption");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+    captionText.innerHTML = dots[slideIndex-1].alt;
+}
