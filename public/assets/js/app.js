4af15c9b8b405b2aa08389df2fcff42ac1433d4f
Ryan Rodwell <ryanrodwell@gmail.com>
2017-10-06 11:59:03 -0400

    updating images for all packages

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -156,11 +156,11 @@ $(document).ready(function() {
                 altName: "Simply Modern - "
             },
             woodStone: {
-                imgList: ["SM-1.jpg","SM-2.jpg","SM-3.jpg","SM-4.jpg"],
+                imgList: ["WOODSTONE-1.jpg","WOODSTONE-2.jpg","WOODSTONE-3.jpg","WOODSTONE-4.jpg"],
                 altName: "Wood & Stone"
             },
             craft: {
-                imgList: ["CRAFT-1.jpg","SM-2.jpg","SM-3.jpg","SM-4.jpg"],
+                imgList: ["CRAFT-1.jpg","CRAFT-2.jpg","CRAFT-3.jpg","CRAFT-4.jpg","CRAFT-5.jpg","CRAFT-6.jpg"],
                 altName: "Craft 2017"
             }
         },
@@ -171,15 +171,15 @@ $(document).ready(function() {
                 altName: "Garden Grill"
             },
             prime: {
-                imgList: ["REMIXA-1.jpg","REMIXA-2.jpg","REMIXA-3.jpg","REMIXA-4.jpg"],
+                imgList: ["Improved-1.jpg","Improved-2.jpg","Improved-3.jpg","Improved-4.jpg","Improved-5.jpg"],
                 altName: "Prime"
             },
             improved: {
-                imgList: ["REMIXB-1.jpg","REMIXB-2.jpg","REMIXB-3.jpg","REMIXB-4.jpg"],
+                imgList: ["Improved-1.jpg","Improved-2.jpg","Improved-3.jpg","Improved-4.jpg","Improved-5.jpg"],
                 altName: "Improved"
             },
             enhanced: {
-                imgList: ["REMIXC-1.jpg","REMIXC-2.jpg","REMIXC-3.jpg","REMIXC-4.jpg"],
+                imgList: ["Improved-1.jpg","Improved-2.jpg","Improved-3.jpg","Improved-4.jpg","Improved-5.jpg"],
                 altName: "Enhanced"
             }
         },
@@ -233,8 +233,8 @@ $(document).ready(function() {
             buildModal(imgSrc, alt, imgPath);
 
         } else if(classNeeded === "craft") {
-            imgSrc = packages.mcdonalds.woodStone.imgList;
-            alt = packages.mcdonalds.woodStone.altName;
+            imgSrc = packages.mcdonalds.craft.imgList;
+            alt = packages.mcdonalds.craft.altName;
             buildModal(imgSrc, alt, imgPath);
 
         }
